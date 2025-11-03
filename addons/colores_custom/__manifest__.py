2d7c333e07d6bb1f93be510b5726e998c030b52e
EmilioArreola <emilioabrham153@gmail.com>
2025-11-03 15:50:52 -0600

    Colores  arreglados

---
+++ addons/colores_custom/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Colores Custom',
+    'version': '1.0',
+    'category': 'Theme',
+    'summary': 'Cambia el color del navbar y elimina el morado de Odoo',
+    'author': 'Abraham',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'colores_custom/static/src/css/colores.css',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}

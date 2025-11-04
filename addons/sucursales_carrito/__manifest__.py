e8eb48f6c4a934cd045d9ed32d1637a9351f80a3
CarlaArreola <carla2ab@hotmail.com>
2025-11-04 13:11:59 -0600

    zzzz

--- addons/sucursales_carrito/__manifest__.py
+++ addons/sucursales_carrito/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Sucursales Carrito (Checkout)",
     'summary': "Selector de sucursales en checkout para Odoo 18",
-    'author': "carlita6.1",
+    'author': "carlitazz",
     'version': '18.0.1.0',
     'category': 'Website/eCommerce',
     

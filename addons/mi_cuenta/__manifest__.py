b8a32e24a4eac8bcab8d9fe527eb7e2fac2c205f
CarlaArreola <carla2ab@hotmail.com>
2026-01-06 17:47:23 -0600

    1.1

--- addons/mi_cuenta/__manifest__.py
+++ addons/mi_cuenta/__manifest__.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Personalización de información del cliente en Mi cuenta",  # Puedes poner el nombre que quieras
+    'name': "Personalización de información del cliente en Mi cuenta",
 
     'summary': """
         Modifica los datos que le pide al cliente en su portal de usuario Mi cuenta.""",
 
     'description': """
         Este módulo hereda la vista del portal de cliente, elimina
-        el selector de envío de facturasy agrega campos adicionales
-        para que el cliente pueda subir su informaciópn de facuraciónn
+        el selector de envío de facturas y agrega campos adicionales
+        para que el cliente pueda subir su información de facturación
         directamente desde su portal de usuario.
     """,
     'author': "Maharba",
     'category': 'Website/Portal',
-    'version': '1.0',
+    'version': '1.1',
 
     # DEPENDENCIAS:
-    # Es VITAL poner 'portal' porque vamos a modificar una vista de ese módulo.
-    # Si no lo pones, Odoo no encontrará la vista 'portal.portal_my_details'.
-    'depends': ['portal','account'],
+    # CORRECCIÓN IMPORTANTE: Agregamos 'account_edi' porque tu XML hereda de él.
+    'depends': ['portal', 'account', 'account_edi'],
 
     # ARCHIVOS DE DATOS:
-    # Aquí registramos el XML que creaste.
     'data': [
         'views/portal_templates.xml',
     ],

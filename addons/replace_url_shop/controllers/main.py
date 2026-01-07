6db01efb9323235b4ea1b14e61a401509825ad7b
CarlaArreola <carla2ab@hotmail.com>
2026-01-07 16:34:47 -0600

    rus 1.3

--- addons/replace_url_shop/controllers/main.py
+++ addons/replace_url_shop/controllers/main.py
@@ -9,8 +9,12 @@ class WebsiteSaleCatalogo(WebsiteSale):
         '/catalogo/category/<model("product.public.category"):category>',
         '/catalogo/category/<model("product.public.category"):category>/page/<int:page>',
     ], type='http', auth="public", website=True, sitemap=True)
-    def shop_catalogo(self, page=0, category=None, search='', **kw):
-        return super().shop(page=page, category=category, search=search, **kw)
-    @http.route('/catalogo/<model("product.template"):product>', type='http', auth="public", website=True, sitemap=True)
+    def shop_catalogo(self, **kw):
+        # Odoo 18 SOLO acepta **kw
+        return super().shop(**kw)
+
+    @http.route([
+        '/catalogo/<model("product.template"):product>',
+    ], type='http', auth="public", website=True, sitemap=True)
     def product_catalogo(self, product, **kw):
-        return super().product(product=product, **kw)
+        return super().product(product, **kw)

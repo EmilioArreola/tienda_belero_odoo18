5ea7016a3601d9f364e617803ab8c7e254541728
CarlaArreola <carla2ab@hotmail.com>
2026-01-07 16:14:14 -0600

    rus 1.1

--- addons/replace_url_shop/controllers/main.py
+++ addons/replace_url_shop/controllers/main.py
@@ -7,6 +7,10 @@ class WebsiteSaleCatalogo(WebsiteSale):
         '/catalogo',
         '/catalogo/page/<int:page>',
         '/catalogo/category/<model("product.public.category"):category>',
+        '/catalogo/category/<model("product.public.category"):category>/page/<int:page>',
     ], type='http', auth="public", website=True, sitemap=True)
-    def shop_catalogo(self, **kw):
-        return super().shop(**kw)
+    def shop_catalogo(self, page=0, category=None, search='', **kw):
+        return super().shop(page=page, category=category, search=search, **kw)
+    @http.route('/catalogo/<model("product.template"):product>', type='http', auth="public", website=True, sitemap=True)
+    def product_catalogo(self, product, **kw):
+        return super().product(product=product, **kw)

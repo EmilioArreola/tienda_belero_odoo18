ea03955b3360be99339e0f39ca5c3c539a76e655
CarlaArreola <carla2ab@hotmail.com>
2026-01-07 13:24:54 -0600

    icon 1.1.2

--- addons/mi_cuenta/__manifest__.py
+++ addons/mi_cuenta/__manifest__.py
@@ -8,7 +8,7 @@
         - Añade campos para RFC (vat) y Régimen Fiscal.
         - Cambia la etiqueta 'Nombre' por 'Razón Social'.
     """,
-    'author': "Abraham y Carla 1.1.1",
+    'author': "1.1.2",
     'category': 'Website/Portal',
     'version': '1.1',
     
@@ -21,7 +21,7 @@
     'data': [
         'views/portal_templates.xml',
     ],
-    
+    'icon': '/mi_cuenta/static/description/icon.png',
     'installable': True,
     'application': False, # False porque es un módulo técnico/extensión, no una App principal
     'license': 'LGPL-3',

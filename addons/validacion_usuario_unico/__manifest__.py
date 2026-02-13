de2f7d4f0f1345560d16ed372149e6f55c3c57f7
EmilioArreola <emilioabrham153@gmail.com>
2026-02-13 12:28:20 -0600

    Validación de usuarios repetidos

---
+++ addons/validacion_usuario_unico/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Validación de Usuarios Únicos',
+    'version': '18.0.1.0.0',
+    'summary': 'Evita la creación de usuarios con datos duplicados (Email/DNI)',
+    'category': 'Administration',
+    'author': 'Tu Nombre o Empresa',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
